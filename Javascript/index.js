7e0447cc96d58c49a73ec9512e64245af6340e0a
Cs12krisz <90339656+Cs12krisz@users.noreply.github.com>
2022-03-22 11:38:48 +0100

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,30 @@
+var tomb=[
+    {
+        nev:'Zoli',
+        jelszo=1234
+    },
+    {
+        nev:'peter',
+        jelszo=2345
+    }
+];
+    document.getElementById('button1').onclick-function() {
+
+    var name=document.getElementById('text1').value;
+    var pass=document.getElementById('pass1').value;
+
+    for (let ){
+        if (iternator nev pass) {
+            var
+        }
+      }
+       if (van == true) {
+           console.log("Ilyen fahasználó van!")
+           
+       } else {
+        console.log("Ilyen felhasználó nincs!")
+     }
+       
+    }
+
+   
