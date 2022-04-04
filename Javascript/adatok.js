aca1e50bb81a1aaee427fb1040b03036b1d31513
Cs12krisz <90339656+Cs12krisz@users.noreply.github.com>
2022-04-04 11:43:21 +0200

    Add files via upload

---
+++ Javascript/adatok.js
@@ -0,0 +1,25 @@
+var termek01 = 15500
+var termek03 = 6999
+var termek03 = 29880
+
+function hozzaad (termek){
+    if(hozzaad('termek01'))
+    localStorage.setItem("termek01"+ ""+db)
+}
+
+function torles (){
+    localStorage.clear
+}
+
+function db(darab){
+    if(hozzaad('termek01')){
+     darab = prompt("Mennyit kérsz?")
+    }
+
+}
+
+
+function tartalom(){
+    var tabla ="<tr><th>"
+    var 
+}
