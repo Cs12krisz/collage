f637b4ca50b6d496cfbeb529d11cf8396db0557a
Cs12krisz <90339656+Cs12krisz@users.noreply.github.com>
2022-09-08 21:08:50 +0200

    Add files via upload

--- Programozasi alap/elso script.py
+++ Programozasi alap/elso script.py
@@ -1,42 +1,19 @@
-import time
-i = 0
-
 """
+import time
+
 while i <= 10:
     print("Hülye mindenki", end = '') #v, b, r, t
     time.sleep(1) #másodperc
     i += 1
 """
+import time
+i = 0
 
 for x in range(0, 3601):
     print(i, ':', x)
-    if(x == 60):
-       i=i+1
-       x=x-60
-    if(x == 120):
-      i=i+1
-      x=x-60
-    if(x == 180):
-      i=i+1
-      x=x-60
-    if(x == 240):
-      i=i+1
-      x=x-60
-    if(x == 300):
-      i=i+1
-      x=x-60
-    if(x == 360):
-      i=i+1
-      x=x-60
-    if(x == 420):
-      i=i+1
-      x=x-60
-    if(x == 480):
-      i=i+1
-      x=x-60
-    if(x == 540):
-      i=i+1
-      x=x-60
-    if(x == 600):
-      i=i+1
-      x=x-60
+    time.sleep(0.25)
+    e = x % 60
+    if 60 > x:
+        i = 0
+    elif e == 0:
+        i+=1
