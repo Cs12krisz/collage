828c4b2d2c8d663b263238c3d2b57c974cb4c8b4
Cs12krisz <90339656+Cs12krisz@users.noreply.github.com>
2022-09-09 11:26:49 +0200

    Add files via upload

---
+++ Programozasi alap/matek.py
@@ -0,0 +1,35 @@
+"""
+a = 3       # a = "Pisti"
+b = 4       # b = "6"
+c = a+b     # c = a+b
+c = a-b     # print(c)
+print(c)
+print(a*b)
+print(a/b)
+"""
+
+"""
+penz = 1000000000
+print("megyek")
+print("megyek")
+if penz>50:
+    print("bót")
+    print("nem vagyok már éhes")
+    if penz>100:
+        print("Csoki")
+        if penz>1000000:
+            print("Ferrari")
+if penz>1000000:            
+    print("vezetek")
+else:
+    print("megyek")
+"""
+
+
+db1 = 10
+db2 = 5
+
+for x in range(db1):
+    for y in range(db2):
+        print(x, ' ', y)
+        
